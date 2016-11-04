35a99ed6821977481cf93022064a2340a139e74a
lululauren <lauren.kemp112@gmail.com>
2016-11-03 19:35:47 -0700

    Initial Commit

---
+++ selfigramTests/selfigramTests.swift
@@ -0,0 +1,36 @@
+//
+//  selfigramTests.swift
+//  selfigramTests
+//
+//  Created by Lauren  Kemp on 2016-11-03.
+//  Copyright © 2016 Lauren  Kemp. All rights reserved.
+//
+
+import XCTest
+@testable import selfigram
+
+class selfigramTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
